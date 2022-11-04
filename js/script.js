f63030cf2e3f230a322ba2da5615bb87beb08469
Lukasz Szmytka <szmytka983@gmail.com>
2022-11-04 17:09:36 +0100

    binding events moove to separate function

--- js/script.js
+++ js/script.js
@@ -10,10 +10,7 @@ const tasks = [
 ];
 
 const addNewTask = (newTaskContent) => {
-  tasks.push({
-    content: newTaskContent,
-  });
-
+  tasks.push({ content: newTaskContent });
   render();
 };
 
@@ -27,9 +24,24 @@ const toggleTaskDone = (taskIndeks) => {
   render();
 };
 
+const bindEvents = () => {
+  const removeButtons = document.querySelectorAll(".js-remove");
+  removeButtons.forEach((removeButton, index) => {
+    removeButton.addEventListener("click", () => {
+      removeTask(index);
+    });
+  });
+
+  const toggleDoneButtons = document.querySelectorAll(".js-done");
+  toggleDoneButtons.forEach((toggleDoneButton, index) => {
+    toggleDoneButton.addEventListener("click", () => {
+      toggleTaskDone(index);
+    });
+  });
+};
+
 const render = () => {
   let htmlString = "";
-
   for (const task of tasks) {
     htmlString += `
             <li
@@ -41,44 +53,22 @@ const render = () => {
             </li>
             `;
   }
-
   document.querySelector(".js-tasks").innerHTML = htmlString;
-
-  const removeButtons = document.querySelectorAll(".js-remove");
-
-  removeButtons.forEach((removeButton, index) => {
-    removeButton.addEventListener("click", () => {
-      removeTask(index);
-    });
-  });
-
-  const toggleDoneButtons = document.querySelectorAll(".js-done");
-
-  toggleDoneButtons.forEach((toggleDoneButton, index) => {
-    toggleDoneButton.addEventListener("click", () => {
-      toggleTaskDone(index);
-    });
-  });
+  bindEvents();
 };
 
 const onFormSubmit = (event) => {
   event.preventDefault();
-
   const newTaskContent = document.querySelector(".js-newTask").value.trim();
-
   if (newTaskContent === "") {
     return;
   }
-
   addNewTask(newTaskContent);
 };
 
 const init = () => {
   render();
-
   const form = document.querySelector(".js-form");
-
   form.addEventListener("submit", onFormSubmit);
 };
-
 init();
