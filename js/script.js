b15111cc9e99be8876736e79426111a2ca5fa356
Lukasz Szmytka <szmytka983@gmail.com>
2022-11-04 15:47:11 +0100

    adding new tasks and showing if a task is done

--- js/script.js
+++ js/script.js
@@ -1,41 +1,54 @@
-{
-    
-    
-    
-    
-    
-    
-    const tasks = [
-        {
-            content: "obejrzeć lekcje",
-            done: true,
-        },
-        {
-            content: "wykonać pracę domową",
-            done: false,
-        },
-    ];
-    
-    const render = () => {
-        let htmlString = "";
+const tasks = [
+  {
+    content: "obejrzeć lekcje",
+    done: true,
+  },
+  {
+    content: "wykonać pracę domową",
+    done: false,
+  },
+];
 
-        for (const task of tasks) {
-            htmlString += `
-            <li>
+const render = () => {
+  let htmlString = "";
+
+  for (const task of tasks) {
+    htmlString += `
+            <li${task.done ? ' style="text-decoration: line-through"' : ""}>
               ${task.content}
             </li>
             `;
-        }
+  }
 
-        document.querySelector(".js-tasks").innerHTML = htmlString;
-    };
-    
-    const init = () => {
-        render();
+  document.querySelector(".js-tasks").innerHTML = htmlString;
+};
 
+const addNewTask = (newTaskContent) => {
+  tasks.push({
+    content: newTaskContent,
+  });
 
-    };
+  render();
+};
 
-    init();
+const onFormSubmit = (event) => {
+    event.preventDefault();
 
-}
+    const newTaskContent = document.querySelector(".js-newTask").value.trim();
+
+    if (newTaskContent === "") {
+      return;
+    }
+
+    addNewTask(newTaskContent);
+  }
+
+const init = () => {
+  render();
+
+  const form = document.querySelector(".js-form");
+
+  form.addEventListener("submit", onFormSubmit);
+};
+
+init();
